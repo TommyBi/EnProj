17c91b37e45d679fa7b699d3e59bd6e2add10264
yang.liu <yang.liu.enli@gmail.com>
2020-10-14 20:41:38 +0800

    [add] add video player proj

---
+++ ECP1.3U1SI/videoPlayer/src/view/VideoPlayer.ts
@@ -0,0 +1,19 @@
+namespace game {
+    export class VideoPlayer extends eui.Component {
+        public kComCaption: game.CatpionPlayerCom;
+
+        constructor() {
+            super();
+            this.skinName = "VideoPlayerSkin";
+        };
+
+        protected createChildren() {
+            super.createChildren();
+            this.init();
+        }
+
+        private init(): void {
+            this.kComCaption.load("video");
+        }
+    }
+} 
