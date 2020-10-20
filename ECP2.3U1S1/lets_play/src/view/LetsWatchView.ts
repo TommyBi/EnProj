8369dc6ea8fefcfe80faef4fa19bf9be8107deaa
yang.liu <yang.liu.enli@gmail.com>
2020-10-20 23:02:28 +0800

    [add] add ecp2.3u1s1

---
+++ ECP2.3U1S1/lets_play/src/view/LetsWatchView.ts
@@ -0,0 +1,19 @@
+namespace game {
+    export class LetsWatchView extends eui.Component {
+        public kComVideo: game.VideoControlComponent;
+
+        constructor() {
+            super();
+            this.skinName = "LetsWatchSkin";
+        };
+
+        protected createChildren() {
+            super.createChildren();
+            this.init();
+        }
+
+        private init(): void {
+            this.kComVideo.play("video");
+        }
+    }
+} 
