30ac05dc2fd5a99250d4c36b0f3830fc6b2ca293
yang.liu <yang.liu.enli@gmail.com>
2020-10-22 22:54:11 +0800

    [modify] add new res

---
+++ ECP2.3U1S2/stick_say/src/manager/EventConst.ts
@@ -0,0 +1,9 @@
+namespace game {
+    export class EventConst {
+        public static eventFinishVideoProgress = "eventFinishVideoProgress";
+        public static eventReplay = "eventReplay";
+        public static startComPlayGame = "startComPlayGame";
+        public static timeBarOut = "timeBarOut";
+        public static eventStart = "eventStart";
+    }
+}
