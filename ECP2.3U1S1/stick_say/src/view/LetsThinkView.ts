c604f72514e30c0f69210e2e7cb9a526650e1370
yang.liu <yang.liu.enli@gmail.com>
2020-10-22 21:16:37 +0800

    [add] add stick say

---
+++ ECP2.3U1S1/stick_say/src/view/LetsThinkView.ts
@@ -0,0 +1,28 @@
+namespace game {
+    export class LetsThinkView extends eui.Component {
+
+        public kImgBg: eui.Image;
+        public kGrpCom: eui.Group;
+
+        private mSkinType: number = 1;
+        private mWords: string[] = ["aaa", "bbb", "ccc", "ddd"];
+        constructor() {
+            super();
+            this.skinName = "LetsThinkSkin";
+        };
+
+        protected createChildren() {
+            super.createChildren();
+            this.init();
+        }
+
+        private init(): void {
+            this.kImgBg.source = `img_bg_${this.mSkinType}_png`;
+            for (let i = 0; i < this.mWords.length; i++) {
+                let com = new ThinkComponent(i, this.mWords[i]);
+                com.scaleX = com.scaleY = this.mWords.length > 3 ? 0.7 : 1;
+                this.kGrpCom.addChild(com);
+            }
+        }
+    }
+} 
