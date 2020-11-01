ef3753f79ebd20a2a65b61baf62fc8e37cb13633
yang.liu <yang.liu.enli@gmail.com>
2020-11-01 11:50:06 +0800

    [modify] modify stick say1

--- 代码模板/lets_play/src/publicCom/publicInterFace.ts
+++
@@ -1,9 +0,0 @@
-namespace XDFFrame {
-    export interface IEffect {
-        //开始效果
-        start(target: egret.DisplayObject, args?: any): void;
-
-        //停止效果
-        stop(target: egret.DisplayObject, args?: any): void;
-    }
-}
